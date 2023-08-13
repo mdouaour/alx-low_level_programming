1e9d74ddff75181dc10b4b61bd16ac7e9f6b9552
mdouaour <yacine23i@hotmail.com>
2023-08-13 05:56:41 -0700

    wrote a c program that prints a string using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+ *main - Entry point
+ *Description: 'a script that prints a string with puts function'
+ *Return: Always 0 (Succes)
+ */
 
 int main(void)
 {
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
