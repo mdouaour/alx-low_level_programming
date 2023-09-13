e660ba324327ca414bdde1694265042909651441
mdouaour <yacine23i@hotmail.com>
2023-09-13 13:16:44 -0700

    added  checks

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,13 +15,9 @@ int	int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (size <= 0)
+	if ((array == NULL) || (cmp == NULL) || (size <= 0))
 	{
 		return (-1);
-	}
-	if ((array == NULL) || (cmp == NULL))
-	{
-		return (0);
 	}
 	while (i < size)
 	{
