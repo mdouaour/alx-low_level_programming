c76880af4de8987ab474646f8c82c5bf127ef1ef
MYacinedouaouria <yacine23i@hotmail.com>
2023-10-10 12:48:11 -0700

    wrote a program that prints the name of the file it was compiled from, followed by a new line

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *main - prints the name of the file it was compiled from
+ *Return:always 0 sucess
+ *
+ *
+ *
+ */
+int	main()
+{
+	int i = 0;
+
+	while (__FILE__[i] != '\0')
+	{
+		_putchar(__FILE__[i]);
+		i++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
