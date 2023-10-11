41c4a338c38218491145c0d1378261f896d5484f
MYacinedouaouria <yacine23i@hotmail.com>
2023-10-11 16:14:47 -0700

    defined a typedef size_t

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef FUN_P
 #define FUN_P
 #define NULL ((void *)0)
+typedef unsigned int long size_t;
 void	print_name(char *name, void (*f)(char *));
 void	array_iterator(int *array, size_t size, void (*action)(int));
 #endif
