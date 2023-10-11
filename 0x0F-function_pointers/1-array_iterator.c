1634bfa577378fb6366d2fa64244a6e5b21ac182
MYacinedouaouria <yacine23i@hotmail.com>
2023-10-11 16:07:09 -0700

    wrote a function that execute a function at each element of an array of integers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,28 @@
+#include <stddef.h>
+#include "function_pointers.h"
+/**
+ *array_iterator - executes a function on each element of an array
+ *@array:an array of integers
+ *@size:the size of the array
+ *@action:a pointer to a function tha accept an int as parameter and retrn void
+ *
+ *
+ *
+ */
+void	array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int i = 0;
+
+	if ((array == NULL) || (action == NULL))
+	{
+		return;
+	}
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+
+
+
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,4 +2,5 @@
 #define FUN_P
 #define NULL ((void *)0)
 void	print_name(char *name, void (*f)(char *));
+void	array_iterator(int *array, size_t size, void (*action)(int));
 #endif
