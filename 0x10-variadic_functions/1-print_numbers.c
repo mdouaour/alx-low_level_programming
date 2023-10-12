86da832ec61c62a95489a8b6cb183dad3a3f8500
MYacinedouaouria <yacine23i@hotmail.com>
2023-10-12 13:16:41 -0700

    Wrote a function that prints numbers, followed by a new line

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ *print_numbers - prints numbers
+ *@separator:string to be printed between numbers
+ *@n: the number of integers passed to the function
+ *
+ */
+
+void	print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list nbrs;
+
+	if (n == 0)
+	{
+		return;
+	}
+	va_start(nbrs, n);
+	while (i < (n - 1))
+	{
+		printf("%d", va_arg(nbrs, int));
+		if (separator != NULL)
+		{
+			printf("%s", separator);
+			i++;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	printf("%d\n", va_arg(nbrs, int));
+	va_end(nbrs);
+}
