d20d9d07bb334224cb87fc5ebd80b7a531f0ab04
MYacinedouaouria <yacine23i@hotmail.com>
2024-01-15 07:02:31 -0800

    added definition of the function insert_dnodeint_at_index that inserts a new node at a given position

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ *insert_dnodeint_at_index - inserts a node at a given index
+ *@h: points to a pointer to a dlistint_t
+ *@idx: the index of the new node
+ *@n: an integer , the data of the node
+ *Return: a pointer to the new node or NULL
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *current = *h, *new, *pr, *nxt;
+	unsigned int i = 1;
+
+	if (idx == 0)
+	{
+		new = add_dnodeint(h, n);
+		return (new);
+	}
+	while ((i <= idx) && (current != NULL))
+	{
+		if (i == idx)
+		{
+			new = malloc(sizeof(dlistint_t));
+			if (new == NULL)
+			{
+				return (NULL);
+			}
+			new->n = n;
+			pr = current->prev;
+			nxt = current->next;
+			pr->next = new;
+			new->prev = pr;
+			new->next = nxt;
+			nxt->prev = new;
+			return (new);
+		}
+		i++;
+		current = current->next;
+	}
+	if ((current == NULL) && (idx == i))
+	{
+		new = add_dnodeint_end(h, n);
+		return (new);
+	}
+	return (NULL);
+
+
+}
