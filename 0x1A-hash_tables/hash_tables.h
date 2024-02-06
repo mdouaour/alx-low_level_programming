6a5afacb92b3b95d5ab8d8ee54718eee8845860f
MYacinedouaouria <yacine23i@hotmail.com>
2024-02-06 07:47:04 -0800

    added data structures for this project

---
+++ 0x1A-hash_tables/hash_tables.h
@@ -0,0 +1,32 @@
+#ifndef HASH_TABLES
+#define HASH_TABLES
+/**
+ *struct hash_node_s - node of a hash table
+ *
+ *@key: the key, string
+ *the key is unique in the hash table
+ *@value: the value coresponding to the key
+ *@next: A pointer to the next node of the list
+ *
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+/**
+ *struct hash_table_s - hash table data structure
+ *
+ *@size: the size of the array
+ *@array: An array of size @size
+ *Each cell of this aray is array is a pointer to the first node of a linkedlis
+ *because we want our HashTable to use chaning collision handeling
+ */
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_s **array;
+} hash_node_t;
+#endif
